d3609c47ae0246a53f1b9777934553f6dc2940b8
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-19 13:54:04 -0400

    Models created

---
+++ Models/Pets.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TamagotchiAPI.Models
+{
+    public class Pets
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int HungerLevel { get; set; }
+        public int HappinessLevel { get; set; }
+    }
+}
